74a93a14681e34e80400fe161989e3c1f9da16f8
Rohit <rakapare86@gmail.com>
2025-11-09 00:39:04 +0530

    Initial commit

---
+++ src/main/java/com/rohit/grocery/groceryapi/GroceryapibackendApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.grocery.groceryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryapibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryapibackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohit/grocery/groceryapi/GroceryapibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohit.grocery.groceryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryapibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
